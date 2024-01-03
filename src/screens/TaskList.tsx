94aa96fdaf9ba6ff8d72c8ec7de80b048443f24d
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-01-03 15:07:21 -0300

    configurando background image

--- src/screens/TaskList.tsx
+++ src/screens/TaskList.tsx
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import todayImagem from '../../assets/imgs/today.jpg';
 
 export default class TaskList extends Component {
   render() {
     return (
-      <SafeAreaView>
-        <Text>TaskList</Text>
+      <SafeAreaView style={styles.container}>
+        <ImageBackground source={todayImagem} style={styles.background} />
+        <View style={styles.taskContainer}>
+          <Text>TaskList</Text>
+        </View>
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 3,
+  },
+  taskContainer: {
+    flex: 7,
+  },
+});
